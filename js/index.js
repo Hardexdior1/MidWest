294862061b76e018046bf53211e21f47d2c2e604
adeniran <adeniranquwam001@gmail.com>
2023-02-08 23:08:12 +0100

    ur papa commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let ul =document.querySelector('.ul')
+let menu=document.querySelector('.menu')
+let header=document.querySelector('.header')
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY >650){
+        header.style.position='fixed'
+        header.style.left=0
+        header.style.right=0
+        header.style.transition='0.3s'
+        header.classList.add('transition');
+
+
+    }
+    else{
+        header.style.position=''
+    }
+
+})
+menu.addEventListener('click',()=>{
+    ul.classList.toggle('active')
+    menu.style.border='1px solid grey'
+    // padding-bottom: 40%;
+
+})
+
