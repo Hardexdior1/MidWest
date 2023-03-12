36e22d787d5d084cda3d2884d62a935f65166d2e
adeniran <adeniranquwam001@gmail.com>
2023-03-12 23:18:20 +0100

    commitee

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 let ul =document.querySelector('.ul')
 let menu=document.querySelector('.menu')
 let header=document.querySelector('.header')
-
 window.addEventListener('scroll',()=>{
     if(window.scrollY >650){
         header.style.position='fixed'
@@ -9,18 +8,12 @@ window.addEventListener('scroll',()=>{
         header.style.right=0
         header.style.transition='0.3s'
         header.classList.add('transition');
-
-
     }
     else{
         header.style.position=''
     }
-
 })
 menu.addEventListener('click',()=>{
     ul.classList.toggle('active')
     menu.style.border='1px solid grey'
-    // padding-bottom: 40%;
-
 })
-
